f51b76ce935d8e857c32ce2df952d5be5a40c170
Hemanth-kumar123 <88092761+Hemanth-kumar123@users.noreply.github.com>
2024-06-21 12:33:24 +0530

    Update StudentGradeEvaluation.java

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -4,21 +4,32 @@ import java.util.Scanner;
 
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String s=sc.nextLine();
-        System.out.println("Name: " +s);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        int age = scanner.nextInt();
+        int score = scanner.nextInt();
+        String grade ;
+        scanner.close(); // Close the scanner
 
-        System.out.print("Enter your Age: ");
-        int age = sc.nextInt();
-        System.out.println("Age: " +age);
+        if (score >= 90) {
+            grade = "A";
+            //System.out.println("A");
+        } else if (score >= 80) {
+            grade = "B";
+           // System.out.println("B");
+        } else if (score >= 70) {
+            grade = "C";
+         //   System.out.println("C");
+        } else if (score >= 60) {
+            grade = "D";
+           // System.out.println("D");
+        } else {
+            grade ="Fail";
+        }
 
-        System.out.println("Enter your exam score: ");
-        int grade = sc.nextInt();
-        System.out.println("score: " +grade);
-        
-        String sr=sc.nextLine();
-        System.out.println("Grade: A" +sr);
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("score: " + score);
+        System.out.println("grade: " + grade);
     }
 }
-
